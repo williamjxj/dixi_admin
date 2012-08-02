3ecf2cc33e307496ee940c20de7682399b65e92f
williamjxj <williamjxj@ip-50-63-185-169.ip.secureserver.net>
2012-08-02 12:11:43 -0700

    update

--- login.php
+++ login.php
@@ -10,51 +10,30 @@ if(isset($_COOKIE['admin']['path']) && (!empty($_COOKIE['admin']['path'])))
 	$config['path'] = SITEROOT.'themes/'.$_COOKIE['admin']['path'].'/';
 else 
 	$config['path'] = SITEROOT.'themes/default/';
-$config['list'] = get_list_defs($config['path']);
 
 class Login extends BaseClass
 {
 	var $project;
 	function __construct(){
 		parent::__construct();
-		$this->project = $this->get_basename();
+		$this->project = PACKAGE;
+		$this->base = $config['path'];
 	}
-	function get_basename() {
-		return basename(getcwd());
-	}
-	function get_userid() {
-		return 'userid';
-		return $this->project.'_userid'; // return 'demo_userid.
-	}
-	function get_username() {
-		return 'username';
-		return $this->project.'_username'; // return 'demo_username.
-	}
-	function get_userpass() {
-		return 'userpass';
-		return $this->project.'_userpass'; // return 'demo_userpass, for cookie.
-	}
-	function get_userlevel() {
-		return 'userlevel';
-		return $this->project.'_userlevel'; // return 'demo_userlevel for left.tpl.html.
-	}
-	function initial() {
-	global $config;
+	public function initial() {
+		global $config;
 ?>
 <!DOCTYPE HTML>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?=$config['header']['title'];?></title>
-<link rel="stylesheet" type="text/css" href="css/login.css"/>
-<link rel="stylesheet" type="text/css" href="<?=$config['path'];?>css/pams-style.css"/>
-<link rel="stylesheet" type="text/css" href="<?=$config['path'];?>css/validationEngine.jquery.css" />
-<script language="javascript" type="text/javascript" src="<?=$config['path'];?>js/jquery-1.5.1.min.js"></script>
-<script language="javascript" type="text/javascript" src="<?=$config['path'];?>js/cookie.js"></script>
-<script language="javascript" type="text/javascript" src="<?=$config['path'];?>js/jquery.validationEngine-en.js"></script>
-<script language="javascript" type="text/javascript" src="<?=$config['path'];?>js/jquery.validationEngine.js"></script>
-<script language="javascript" type="text/javascript" src="include/init.js"></script>
-<script language="javascript" type="text/javascript">
+<link rel="stylesheet" type="text/css" href="login/login.css"/>
+<link rel="stylesheet" type="text/css" href="include/validationEngine/validationEngine.jquery.css" />
+<script type="text/javascript" src="include/js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="include/js/cookie.js"></script>
+<script type="text/javascript" src="include/validationEngine/jquery.validationEngine-en.js"></script>
+<script type="text/javascript" src="include/validationEngine/jquery.validationEngine.js"></script>
+<script type="text/javascript">
 $(document).ready(function() {
 var form = $('#login_form');
 form.validationEngine();
@@ -72,7 +51,7 @@ $(form).submit(function(event){
 			},
 			success: function(data) {
 				if(data instanceof Object) {
-					document.location.href='index.php';
+					document.location.href='./index.php';
 				}
 				else {
 					$('#submit').show();
@@ -106,12 +85,11 @@ $('#username').select().focus();
 </head>
 <body>
     <div id="upper">
-        <div class="pamslogo"></div>
+        <div class="dixilogo"></div>
     </div>
     <div class="headerText">
         <h1><?=$config['header']['title'];?></h1>
     </div>
-    <!--div id="st" style="cursor: pointer;">切至繁体版</div-->
 <div id="div_login">
   <form id="login_form" action="<?=$_SERVER['PHP_SELF'];?>" method="post">
     <div class="user">
@@ -131,7 +109,6 @@ $('#username').select().focus();
           <div class="loginButton">
               <input type="submit" id="submit" value="Login" />
               <span id="msg" name="msg" style="display: none"><?=$config['list']['wait1'];?></span>
-              <!--<input type="reset" value="Reset" />-->
           </div>
       </div>
   </form>
@@ -174,48 +151,69 @@ $('#username').select().focus();
 		return false;
 	}
 
-  function update_login_info($username, $uid)  {
-    $ip = $this->get_real_ip();
-	$browser = $this->get_browser();
-	$session = $this->get_session();
-    $query = "insert into login_info(uid,ip,browser,username,session,count,login_time,logout,logout_time, expired)
-      values(".$uid.", '".$ip."', '".$this->mdb2->escape($browser)."', '".$username."', '".$session."', 1, NULL, 'N', '', NOW() + INTERVAL 10 HOUR)
-      on duplicate key update
-      count = count+1,
-	  login_time = NULL,
-	  expired = NOW() + INTERVAL 10 HOUR,
-	  session = '".$session."', 
-      logout='N',
-	  logout_time=''";
-	$affected = $this->mdb2->exec($query);
-	if (PEAR::isError($affected)) {
-		die($affected->getMessage());
+	function update_login_info($username, $uid)  {
+		$ip = $this->get_real_ip();
+		$browser = $this->get_browser();
+		$session = $this->get_session();
+		$query = "insert into login_info(uid,ip,browser,username,session,count,login_time,logout,logout_time, expired)
+		  values(".$uid.", '".$ip."', '".$this->mdb2->escape($browser)."', '".$username."', '".$session."', 1, NULL, 'N', '', NOW() + INTERVAL 10 HOUR)
+		  on duplicate key update
+		  count = count+1,
+		  login_time = NULL,
+		  expired = NOW() + INTERVAL 10 HOUR,
+		  session = '".$session."', 
+		  logout='N',
+		  logout_time=''";
+		$affected = $this->mdb2->exec($query);
+		if (PEAR::isError($affected)) {
+			die($affected->getMessage());
+		}
 	}
-  }
-
-  function update_logout_info() {
-	$query = "update login_info set logout='Y', logout_time=NULL, session=NULL where session='".$this->get_session()."'";
-	$affected = $this->mdb2->exec($query);
-	if (PEAR::isError($affected)) {
-		die($affected->getMessage());
+	
+	function update_logout_info() {
+		$query = "update login_info set logout='Y', logout_time=NULL, session=NULL where session='".$this->get_session()."'";
+		$affected = $this->mdb2->exec($query);
+		if (PEAR::isError($affected)) {
+			die($affected->getMessage());
+		}
 	}
-  }
   
-  function get_browser() {
-	return $_SERVER["HTTP_USER_AGENT"]; //Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13
-  }
-  function get_real_ip() {
-    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
-      $ip=$_SERVER['HTTP_CLIENT_IP'];
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
-      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-    else
-      $ip=$_SERVER['REMOTE_ADDR'];
-    return $ip;
-  }
+	//Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13  
+	private function get_browser() {
+		return $_SERVER["HTTP_USER_AGENT"]; 
+	}
+  
+	private function get_real_ip() {
+		if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+		  $ip=$_SERVER['HTTP_CLIENT_IP'];
+		elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+		  $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+		else
+		  $ip=$_SERVER['REMOTE_ADDR'];
+		return $ip;
+	}
+	
+	// dixitruth['userid'];
+	private function get_userid() {
+		return 'userid';
+	}
+	// dixitruth['username'];
+	private function get_username() {
+		return 'username';
+	}
+	// dixitruth['userpass'];
+	private function get_userpass() {
+		return 'userpass';
+	}
+	private function get_userlevel() {
+		return 'userlevel';
+	}
+
 }
 
+
 //////////////////////////
+
 $login = new Login();
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
