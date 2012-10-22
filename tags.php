ed547e95ed77eca662b830637788b345fff65124
Williamjxj <williamjxj@hotmail.com>
2012-10-21 21:11:52 -0700

    update

--- tags.php
+++ tags.php
@@ -7,42 +7,22 @@ require_once(SITEROOT.'/configs/setting.inc.php');
 require_once(SITEROOT.'/configs/config.inc.php');
 require_once(SITEROOT.'/configs/ListAdvanced.inc.php');
 
-class TagClass extends ListAdvanced
+class KeySearchClass extends ListAdvanced
 {
   var $url, $self, $session;
 
   public function __construct() {
     parent::__construct();
-	$this->table = 'tags';
+	$this->table = 'key_search';
   }
 
   function get_search_form_settings() {
 	return array(
 		array(
 		  'type' => 'text',
-		  'display_name' => '标签:',
-		  'id' => 'tid_s',
-		  'name' => 'tid',
-		),
-		array(
-		  'type' => 'select',
-		  'display_name' => '标签:',
-		  'id' => 'tag_s',
-		  'name' => 'tag',
-		  'call_func' => 'get_tag_options',
-		),
-		array(
-			'type' => 'radio',
-			'display_name' => '状态:',
-			'id' => 'active_s',
-			'name' => 'active',
-			'lists' => array(
-				'N' => '禁止',
-				'Y' => '活动',
-				'A' => '所有',
-			),
-			'checked' => 'A',
-			'ignore' => 'A',
+		  'display_name' => '查询词:',
+		  'id' => 'keyword_s',
+		  'name' => 'keyword',
 		),
 		array(
 			'type' => 'date',
@@ -64,21 +44,22 @@ class TagClass extends ListAdvanced
 	return array(
 		array(
 			'type' => 'text',
-			'display_name' => '标签:',
-			'name' => 'name',
+			'display_name' => '查询词:',
+			'name' => 'keyword',
 		),
 		array(
-			'type' => 'radio',
-			'display_name' => '状态:',
-			'name' => 'active',
-			'lists' => array(
-				'N' => '禁止',
-				'Y' => '活动',
-			),
+			'type' => 'text',
+			'display_name' => '包括:',
+			'name' => 'include',
+		),
+		array(
+			'type' => 'text',
+			'display_name' => '排除:',
+			'name' => 'exclude',
 		),
 		array(
 			'type' => 'hidden',
-			'name' => 'tid',
+			'name' => 'kid',
 			'db_type' => 'int',
 		)
 	);
@@ -88,9 +69,9 @@ class TagClass extends ListAdvanced
 	return array(
 		array(
 		  'type' => 'text',
-		  'display_name' => '标签:',
-		  'id' => 'name',
-		  'name' => 'name',
+		  'display_name' => '查询词:',
+		  'id' => 'keyword',
+		  'name' => 'keyword',
 		),
 	);
   }
@@ -99,14 +80,13 @@ class TagClass extends ListAdvanced
   // parse_ini 不支持多国字体。
   function get_header() {
   	return array(
-		'tid' => 'tid',
-		'标签' => 'name',
-		'频率' => 'frequency',
-		'状态' => 'active',
-		'创建日期' => 'created',
+		'kid' => 'kid',
+		'查询词' => 'keyword',
+		'包括' => 'include',
+		'排除' => 'exclude',
 		'创建者' => 'createdby',
+		'创建日期' => 'created',
 		'更新日期' => 'updated',
-		'更新者' => 'updatedby',
 	);
   }  
 }
@@ -116,25 +96,25 @@ class TagClass extends ListAdvanced
 global $config;
 $config['tabs'] = array('1'=>'标签列表', '2'=>'添加标签');
 
-$tag = new TagClass();
+$ks = new KeySearchClass();
 
-if(! $tag->check_access()) {
-  $tag->set_breakpoint();
+if(! $ks->check_access()) {
+  $ks->set_breakpoint();
   echo "<script>if(window.opener){window.opener.location.href='".LOGIN."';} else{window.parent.location.href='".LOGIN."';}</script>";
   exit;
 }
-$tag->get_table_info();
+$ks->get_table_info();
 
-$tag->set_default_config(array('calender'=>true,'jvalidate'=>true));
+$ks->set_default_config(array('calender'=>true,'jvalidate'=>true));
 
-if (isset($_GET['tid'])) {
+if (isset($_GET['kid'])) {
 	switch($_GET['step']) {
 	case 1:
-		$tag->select_pages($_GET['tid']);
+		$ks->select_pages($_GET['kid']);
 		break;
 	case 2:
 		$uid = isset($_GET['uid']) ? $_GET['uid'] : $this->userid;
-		$tag->select_users_pages($uid);
+		$ks->select_users_pages($uid);
 		break;
 	default:
 		break;
@@ -144,88 +124,88 @@ elseif(isset($_POST['update'])) {
 	$ac->update_pages();
 }
 elseif(isset($_GET['js_search_form'])) {
-	$ary = $tag->get_search_form_settings();
-	$tag->assign('search_form', $ary);	
-	$tag->assign('config', $config);
-	$tag->display($config['templs']['search']);
+	$ary = $ks->get_search_form_settings();
+	$ks->assign('search_form', $ary);	
+	$ks->assign('config', $config);
+	$ks->display($config['templs']['search']);
 }
 elseif(isset($_GET['js_edit_form'])) {
-	$ary = $tag->get_edit_form_settings();
-	$record = $tag->get_record($_GET['id']);
-	$tag->assign('record', $record);	
-	if(isset($_GET['tr'])) $tag->assign('form_id', 'ef_'.$_GET['id'].'-'.$_GET['tr']);
-	else $tag->assign('form_id', 'ef_'.$_GET['id']);
-	$tag->assign('edit_form', $ary);	
-	$tag->assign('config', $config);
-	$tag->display($config['templs']['edit']);
+	$ary = $ks->get_edit_form_settings();
+	$record = $ks->get_record($_GET['id']);
+	$ks->assign('record', $record);	
+	if(isset($_GET['tr'])) $ks->assign('form_id', 'ef_'.$_GET['id'].'-'.$_GET['tr']);
+	else $ks->assign('form_id', 'ef_'.$_GET['id']);
+	$ks->assign('edit_form', $ary);	
+	$ks->assign('config', $config);
+	$ks->display($config['templs']['edit']);
 }
 elseif(isset($_GET['js_add_form'])) {
-	$ary = $tag->get_add_form_settings();
-	$tag->assign('add_form', $ary);	
-	$tag->assign('config', $config);
-	$tag->display($config['templs']['add']);
+	$ary = $ks->get_add_form_settings();
+	$ks->assign('add_form', $ary);	
+	$ks->assign('config', $config);
+	$ks->display($config['templs']['add']);
 }
 elseif(isset($_POST['js_edit_column'])) {
-	$ret = $tag->update_column(array('updatedby'=>$tag->username));
+	$ret = $ks->update_column(array('updatedby'=>$ks->username));
 	echo json_encode($ret);
 }
 elseif(isset($_REQUEST['action'])) {
 	switch($_REQUEST['action']) {
 		case 'edit':
-		  $ary = $tag->get_edit_form_settings();
-		  echo json_encode($tag->edit_table($ary));
+		  $ary = $ks->get_edit_form_settings();
+		  echo json_encode($ks->edit_table($ary));
 			break;
 		case 'delete':
-			$tag->delete($_GET['id']);
+			$ks->delete($_GET['id']);
 			break;
 		case 'add':
-			$tag->create(array('createdby'=>$tag->username, 'updatedby'=>$tag->username, 'created'=>'NOW()'));
+			$ks->create(array('createdby'=>$ks->username, 'updatedby'=>$ks->username, 'created'=>'NOW()'));
 			break;    
 		default:
 			break;
 	}
 }
 else if( isset($_POST['search']) || (isset($_GET['page']) && isset($_GET['sort'])) || isset($_GET['page']) || isset($_GET['js_reload_list']) ) {
-	if(isset($_POST['search'])) $tag->parse();
+	if(isset($_POST['search'])) $ks->parse();
 
-	$data = $tag->list_all();
+	$data = $ks->list_all();
 	$data['options'] = array(EDIT, DELETE);
 	
-	$header = $tag->get_header(); //$tag->get_mappings();
+	$header = $ks->get_header(); //$ks->get_mappings();
 
-	$pagination = $tag->draw( $data['current_page'], $data['total_pages'] );
+	$pagination = $ks->draw( $data['current_page'], $data['total_pages'] );
 	
-	$tag->assign('config', $config);
-	$tag->assign('header', $header);
-	$tag->assign('data', $data);
-	$tag->assign("pagination", $pagination);
-	$tpl = $tag->get_html_template();
-	$tag->display($tpl); // not use display, should use AJAX.
+	$ks->assign('config', $config);
+	$ks->assign('header', $header);
+	$ks->assign('data', $data);
+	$ks->assign("pagination", $pagination);
+	$tpl = $ks->get_html_template();
+	$ks->display($tpl); // not use display, should use AJAX.
 }
 // default: list data.
 else {
 
-	if (isset($_SESSION[$tag->self][$tag->session['sql']])) $_SESSION[$tag->self][$tag->session['sql']] = '';
+	if (isset($_SESSION[$ks->self][$ks->session['sql']])) $_SESSION[$ks->self][$ks->session['sql']] = '';
 
-	$total_rows = $tag->get_total_rows($tag->get_count_sql());
+	$total_rows = $ks->get_total_rows($ks->get_count_sql());
 
-	$_SESSION[$tag->self][$tag->session['rows']] = $total_rows < 1 ? 1 : $total_rows;
+	$_SESSION[$ks->self][$ks->session['rows']] = $total_rows < 1 ? 1 : $total_rows;
 	
-	$data = $tag->list_all();
+	$data = $ks->list_all();
 	$data['options'] = array(EDIT, DELETE);
 	
-	$header = $tag->get_header(); //$tag->get_mappings();
+	$header = $ks->get_header(); //$ks->get_mappings();
 
-	$pagination = $tag->draw( $data['current_page'], $data['total_pages'] );
+	$pagination = $ks->draw( $data['current_page'], $data['total_pages'] );
 	
-	$tpl = $tag->get_html_template();
+	$tpl = $ks->get_html_template();
 
-	$tag->assign('config', $config);
-	$tag->assign('header', $header);
-	$tag->assign('data', $data);
-	$tag->assign("pagination", $pagination);
+	$ks->assign('config', $config);
+	$ks->assign('header', $header);
+	$ks->assign('data', $data);
+	$ks->assign("pagination", $pagination);
 
-	$tag->assign("template", $tpl);
-	$tag->display($config['templs']['main']);
+	$ks->assign("template", $tpl);
+	$ks->display($config['templs']['main']);
 }
 ?>
