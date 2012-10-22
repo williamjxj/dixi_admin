99890b1c313449f5ecda255b71896bea2c9c999b
Williamjxj <williamjxj@hotmail.com>
2012-10-22 00:43:37 -0700

    update

--- memcached.php
+++ memcached.php
@@ -5,8 +5,9 @@ $m = new Memcached();
 
 $m->addServer('localhost', 11211);
 
+$t = $t = iconv('UTF-8', 'UTF-8//TRANSLIT', 'Ã÷ÐÇ');
 //  $m->getDelayed(array('keyword', 'include', 'exclude'), true);
-$m->getDelayed(array('Ã÷ÐÇ'), true);
+$m->getDelayed(array($t), true);
 
 echo "<pre>";
 print_r($m->fetchAll());
